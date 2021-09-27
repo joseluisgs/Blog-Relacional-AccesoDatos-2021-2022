08cbe84bd906ac6d160c15fe36b22012ecde02b4
joseuisgs <joseluisgonsan@gmail.com>
2021-09-27 19:19:39 +0200

    Utilidades

--- src/main/java/es/joseluisgs/dam/blog/App.java
+++ src/main/java/es/joseluisgs/dam/blog/App.java
@@ -1,13 +1,24 @@
 package es.joseluisgs.dam.blog;
 
+import es.joseluisgs.dam.blog.utils.ApplicationProperties;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ApplicationProperties properties = new ApplicationProperties();
+        System.out.println("Bienvenid@s a " +
+                properties.readProperty("app.title") + " de " +
+                properties.readProperty("app.curso"));
+
+        Dotenv dotenv = Dotenv.load();
+        System.out.println(dotenv.get("MY_ENV_VAR1"));
+
     }
 }
