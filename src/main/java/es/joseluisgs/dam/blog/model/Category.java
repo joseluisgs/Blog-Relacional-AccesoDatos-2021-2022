5b887c8047782045a453ed0d4dd33c5ec2421844
joseuisgs <joseluisgonsan@gmail.com>
2021-09-28 17:52:41 +0200

    Cambio de ID a Long

--- src/main/java/es/joseluisgs/dam/blog/model/Category.java
+++ src/main/java/es/joseluisgs/dam/blog/model/Category.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Category {
-    private long id;
+    private Long id;
     private String texto;
     // Lista de post que tiene asociado
     private Set<Post> posts = new HashSet<>();
