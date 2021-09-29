11ea0316d699c987d252265f11b9be3237e954af
joseuisgs <joseluisgonsan@gmail.com>
2021-09-29 17:43:15 +0200

    Controlador DB

--- src/main/java/es/joseluisgs/dam/blog/App.java
+++ src/main/java/es/joseluisgs/dam/blog/App.java
@@ -1,7 +1,11 @@
 package es.joseluisgs.dam.blog;
 
 import es.joseluisgs.dam.blog.utils.ApplicationProperties;
+import es.joseluisgs.dam.blog.utils.DataBaseController;
 import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Hello world!
@@ -10,11 +14,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationProperties properties = new ApplicationProperties();
         System.out.println("Bienvenid@s a " +
-                properties.readProperty("app.title") + " de " +
+                properties.readProperty("app.title") + " "
+                + properties.readProperty("app.version") +" de " +
                 properties.readProperty("app.curso"));
 
         Dotenv dotenv = Dotenv.load();
-        System.out.println(dotenv.get("MY_ENV_VAR1"));
+        System.out.println(dotenv.get("DATABASE_NAME"));
+
+        DataBaseController controller = DataBaseController.getInstance();
+        controller.open();
+        ResultSet rs = controller.query("SELECT * from test");
+        try {
+            rs.first();
+            System.out.println(rs.getString(1));
+            controller.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
