cce7da4430a5a43f2451130e5c2933abd3c8f8ac
joseuisgs <joseluisgonsan@gmail.com>
2021-10-04 19:46:21 +0200

    ControllerDB

--- src/main/java/es/joseluisgs/dam/blog/database/DataBaseController.java
+++ src/main/java/es/joseluisgs/dam/blog/database/DataBaseController.java
@@ -45,6 +45,7 @@ public class DataBaseController {
         // Leemos los datos de la base de datos que pueden estar en
         // porperties o en .env
         // imaginemos que el usuario y pasword estaán en .env y el resto en application.properties
+        // si no los rellenamos aquí.
         ApplicationProperties properties = new ApplicationProperties();
         serverUrl = properties.readProperty("database.server.url");
         serverPort = properties.readProperty("database.server.port");
