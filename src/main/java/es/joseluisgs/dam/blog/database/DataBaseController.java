511c198efb682c813b9e465f58ad4cc06db1d87e
joseuisgs <joseluisgonsan@gmail.com>
2021-10-05 23:11:34 +0200

    Documentación

--- src/main/java/es/joseluisgs/dam/blog/database/DataBaseController.java
+++ src/main/java/es/joseluisgs/dam/blog/database/DataBaseController.java
@@ -2,6 +2,7 @@ package es.joseluisgs.dam.blog.database;
 
 import es.joseluisgs.dam.blog.utils.ApplicationProperties;
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.NonNull;
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 import java.io.BufferedReader;
@@ -11,12 +12,20 @@ import java.io.Reader;
 import java.sql.*;
 import java.util.Optional;
 
+/**
+ * Controlador de Bases de Datos Relacionales
+ */
 public class DataBaseController {
     private static DataBaseController controller;
+    @NonNull
     private String serverUrl;
+    @NonNull
     private String serverPort;
+    @NonNull
     private String dataBaseName;
+    @NonNull
     private String user;
+    @NonNull
     private String password;
     /*
     Tipos de Driver
@@ -24,17 +33,27 @@ public class DataBaseController {
     MySQL: "com.mysql.jdbc.Driver"
     MariaDB: com.mysql.cj.jdbc.Driver
      */
+    @NonNull
     private String jdbcDriver;
     // Para manejar las conexiones y respuestas de las mismas
+    @NonNull
     private Connection connection;
+    @NonNull
     private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
 
+    /**
+     * Constructor privado para Singleton
+     */
     private DataBaseController() {
         // System.out.println("Mi nombre es: " + this.nombre);
         initConfig();
     }
 
+    /**
+     * Devuelve una instancia del controlador
+     *
+     * @return instancia del controladorBD
+     */
     public static DataBaseController getInstance() {
         if (controller == null) {
             controller = new DataBaseController();
@@ -42,6 +61,10 @@ public class DataBaseController {
         return controller;
     }
 
+    /**
+     * Carga la configuración de acceso al servidor de Base de Datos
+     * Puede ser directa "hardcodeada" o asignada dinámicamente a traves de ficheros .env o properties
+     */
     private void initConfig() {
         // Leemos los datos de la base de datos que pueden estar en
         // porperties o en .env
@@ -57,7 +80,11 @@ public class DataBaseController {
         password = dotenv.get("DATABASE_PASSWORD");
     }
 
-    // Método para abrir la Conexion la Base de Datos
+    /**
+     * Abre la conexión con el servidor  de base de datos
+     *
+     * @throws SQLException Servidor no accesible por problemas de conexión o datos de acceso incorrectos
+     */
     public void open() throws SQLException {
         //String url = "jdbc:sqlite:"+this.ruta+this.bbdd; //MySQL jdbc:mysql://localhost/prueba", "root", "1daw"
         String url = "jdbc:mariadb://" + this.serverUrl + ":" + this.serverPort + "/" + this.dataBaseName;
@@ -66,12 +93,25 @@ public class DataBaseController {
         connection = DriverManager.getConnection(url, user, password);
     }
 
+    /**
+     * Cierra la conexión con el servidor de base de datos
+     *
+     * @throws SQLException Servidor no responde o no puede realizar la operación de cierre
+     */
     public void close() throws SQLException {
         preparedStatement.close();
         connection.close();
     }
 
-    private ResultSet executeQuery(String querySQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta a la base de datos de manera "preparada" obteniendo los parametros opcionales si son necesarios
+     *
+     * @param querySQL consulta SQL de tipo select
+     * @param params   parámetros de la consulta parametrizada
+     * @return ResultSet de la consulta
+     * @throws SQLException No se ha podido realizar la consulta o la tabla no existe
+     */
+    private ResultSet executeQuery(@NonNull String querySQL, Object... params) throws SQLException {
         preparedStatement = connection.prepareStatement(querySQL);
         // Vamos a pasarle los parametros usando preparedStatement
         for (int i = 0; i < params.length; i++) {
@@ -80,16 +120,41 @@ public class DataBaseController {
         return preparedStatement.executeQuery();
     }
 
-    public Optional<ResultSet> select(String querySQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta select a la base de datos de manera "preparada" obteniendo los parametros opcionales si son necesarios
+     *
+     * @param querySQL consulta SQL de tipo select
+     * @param params   parámetros de la consulta parametrizada
+     * @return ResultSet de la consulta
+     * @throws SQLException No se ha podido realizar la consulta o la tabla no existe
+     */
+    public Optional<ResultSet> select(@NonNull String querySQL, Object... params) throws SQLException {
         return Optional.of(executeQuery(querySQL, params));
     }
 
-    public Optional<ResultSet> select(String querySQL, int limit, int offset, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta select a la base de datos de manera "preparada" obteniendo los parametros opcionales si son necesarios
+     *
+     * @param querySQL consulta SQL de tipo select
+     * @param limit    número de registros de la página
+     * @param offset   desplazamiento de registros o número de registros ignorados para comenzar la devolución
+     * @param params   parámetros de la consulta parametrizada
+     * @return ResultSet de la consulta
+     * @throws SQLException No se ha podido realizar la consulta o la tabla no existe o el desplazamiento es mayor que el número de registros
+     */
+    public Optional<ResultSet> select(@NonNull String querySQL, int limit, int offset, Object... params) throws SQLException {
         String query = querySQL + " LIMIT " + limit + " OFFSET " + offset;
         return Optional.of(executeQuery(query, params));
     }
 
-    public Optional<ResultSet> insert(String insertSQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta de tipo insert de manera "preparada" con los parametros opcionales si son encesarios
+     * @param insertSQL consulta SQL de tipo insert
+     * @param params parámetros de la consulta parametrizada
+     * @return Clave del registro insertado
+     * @throws SQLException tabla no existe o no se ha podido realizar la operación
+     */
+    public Optional<ResultSet> insert(@NonNull String insertSQL, Object... params) throws SQLException {
         // Con return generated keys obtenemos las claves generadas is las claves es autonumerica por ejemplo
         preparedStatement = connection.prepareStatement(insertSQL, preparedStatement.RETURN_GENERATED_KEYS);
         // Vamos a pasarle los parametros usando preparedStatement
@@ -100,15 +165,36 @@ public class DataBaseController {
         return Optional.of(preparedStatement.getGeneratedKeys());
     }
 
-    public int update(String updateSQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta de tipo update de manera "preparada" con los parametros opcionales si son encesarios
+     * @param updateSQL consulta SQL de tipo update
+     * @param params parámetros de la consulta parametrizada
+     * @return número de registros actualizados
+     * @throws SQLException tabla no existe o no se ha podido realizar la operación
+     */
+    public int update(@NonNull String updateSQL, Object... params) throws SQLException {
         return updateQuery(updateSQL, params);
     }
 
-    public int delete(String deleteSQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta de tipo delete de manera "preparada" con los parametros opcionales si son encesarios
+     * @param deleteSQL consulta SQL de tipo delete
+     * @param params parámetros de la consulta parametrizada
+     * @return número de registros eliminados
+     * @throws SQLException tabla no existe o no se ha podido realizar la operación
+     */
+    public int delete(@NonNull String deleteSQL, Object... params) throws SQLException {
         return updateQuery(deleteSQL, params);
     }
 
-    private int updateQuery(String genericSQL, Object... params) throws SQLException {
+    /**
+     * Realiza una consulta de tipo update, es decir que modifca los datos, de manera "preparada" con los parametros opcionales si son encesarios
+     * @param genericSQL consulta SQL de tipo update, delete, creted, etc.. que modifica los datos
+     * @param params parámetros de la consulta parametrizada
+     * @return número de registros eliminados
+     * @throws SQLException tabla no existe o no se ha podido realizar la operación
+     */
+    private int updateQuery(@NonNull String genericSQL, Object... params) throws SQLException {
         // Con return generated keys obtenemos las claves generadas
         preparedStatement = connection.prepareStatement(genericSQL);
         // Vamos a pasarle los parametros usando preparedStatement
@@ -118,7 +204,7 @@ public class DataBaseController {
         return preparedStatement.executeUpdate();
     }
 
-    public void initData(String sqlFile) throws FileNotFoundException {
+    public void initData(@NonNull String sqlFile) throws FileNotFoundException {
         ScriptRunner sr = new ScriptRunner(connection);
         Reader reader = new BufferedReader(new FileReader(sqlFile));
         sr.runScript(reader);
